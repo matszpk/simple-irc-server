426cd3896190603f259e43f0357b567dda5fb8b5
matszpk <matszpk@interia.pl>
2022-05-06 13:23:03 +0200

    Add next testcase for WHOIS - multiple nickmasks.

--- src/state/rest_cmds.rs
+++ src/state/rest_cmds.rs
@@ -1353,4 +1353,52 @@ mod test {
         
         quit_test_server(main_state, handle).await;
     }
+    
+    #[tokio::test]
+    async fn test_command_whois_multiple() {
+        let (main_state, handle, port) = run_test_server(MainConfig::default()).await;
+        
+        {
+            let mut line_stream = login_to_test_and_skip(port, "fanny", "fanny",
+                    "Fanny BumBumBum").await;
+            login_to_test_and_skip(port, "henry", "henry", "Henry Solo").await;
+            login_to_test_and_skip(port, "harry", "harry", "Harry Lazy").await;
+            login_to_test_and_skip(port, "dizzy", "dizzy", "Dizzy Multi").await;
+            
+            time::sleep(Duration::from_millis(50)).await;
+            let (signon, signon2, signon3) = {
+                let state = main_state.state.read().await;
+                (state.users.get("harry").unwrap().signon,
+                        state.users.get("henry").unwrap().signon,
+                        state.users.get("dizzy").unwrap().signon)
+            };
+            
+            line_stream.send("WHOIS *ry,dizzy".to_string()).await.unwrap();
+            let mut expecteds = [ ":irc.irc 311 fanny harry ~harry 127.0.0.1 * :Harry Lazy",
+                ":irc.irc 312 fanny harry irc.irc :This is IRC server",
+                &format!(":irc.irc 317 fanny harry {} {} :seconds idle, signon time",
+                            SystemTime::now().duration_since(UNIX_EPOCH)
+                                .unwrap().as_secs() - signon, signon),
+                ":irc.irc 311 fanny henry ~henry 127.0.0.1 * :Henry Solo",
+                ":irc.irc 312 fanny henry irc.irc :This is IRC server",
+                &format!(":irc.irc 317 fanny henry {} {} :seconds idle, signon time",
+                            SystemTime::now().duration_since(UNIX_EPOCH)
+                                .unwrap().as_secs() - signon2, signon2),
+                ":irc.irc 311 fanny dizzy ~dizzy 127.0.0.1 * :Dizzy Multi",
+                ":irc.irc 312 fanny dizzy irc.irc :This is IRC server",
+                &format!(":irc.irc 317 fanny dizzy {} {} :seconds idle, signon time",
+                            SystemTime::now().duration_since(UNIX_EPOCH)
+                                .unwrap().as_secs() - signon3, signon3),
+                 ].iter().map(|x| x.to_string()).collect::<Vec<_>>();
+            expecteds.sort();
+            let mut results = vec![];
+            for _ in 0..9 { results.push(line_stream.next().await.unwrap().unwrap()); }
+            results.sort();
+            assert_eq!(expecteds, results);
+            assert_eq!(":irc.irc 318 fanny *ry,dizzy :End of /WHOIS list".to_string(),
+                    line_stream.next().await.unwrap().unwrap());
+        }
+        
+        quit_test_server(main_state, handle).await;
+    }
 }
